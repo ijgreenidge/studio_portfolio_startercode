3329c6b71fcdca46ed2d9e463a6673013f56c37a
Imani Greenidge <igreenidge2018@bklawtech.com>
2017-11-19 19:18:50 +0000

    some projects added

--- js/script.js
+++ js/script.js
@@ -6,3 +6,23 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
+  var config = {
+    apiKey: "AIzaSyATiRsiPRE7RDRnVoVeOqpBFWuc7po54SM",
+    authDomain: "my-portfolio-cb0ba.firebaseapp.com",
+    databaseURL: "https://my-portfolio-cb0ba.firebaseio.com",
+    projectId: "my-portfolio-cb0ba",
+    storageBucket: "",
+    messagingSenderId: "655685115416"
+  };
+  firebase.initializeApp(config);
+  
+  
+  var firstName = firebase.database().ref("info.first-name");
+  
+  firstName.on('value', function(snapshot){
+     console.log(snapshot.val()); 
+     $(".name").html(snapshot.val());
+  });
+  
+  
+  
